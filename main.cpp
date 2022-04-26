4f6dc005bed05dbc3aec7042ba8fc190ba31d150
LirenFox <104010604+LirenFox@users.noreply.github.com>
2022-04-26 19:53:35 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,14 +10,10 @@ int main()
     vector<int> arr;
     int temp;
     while(cin >> temp){
-        c=cin.get();
         arr.push_back(temp);
-        if(c=='\n'){
-            for(int i=arr.size()-1; i>=0; i--){
-                cout << arr[i]<< ' ';
-            }
-            arr.clear();
-        }
     }
+    for(int i=arr.size()-1; i>=1; i--)
+                cout << arr[i]<< " ";
+            cout << arr[0]<< endl;
     return 0;
 }
